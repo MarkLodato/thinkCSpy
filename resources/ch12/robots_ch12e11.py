5224fba685928f278c4687f0fcd839748230da8f
Jeffrey Elkner <jelkner@robeson>
2008-11-16 11:18:47 -0500

    added ex. 11 to chapter 12 and 'Adding junk' section.

--- resources/ch12/robots_ch12e11.py
+++ resources/ch12/robots_ch12e11.py
@@ -63,13 +63,13 @@ def move_player(player):
     return False
 
 
-def collided(robot, player):
-    return player['x'] == robot['x'] and player['y'] == robot['y']
+def collided(thing1, thing2):
+    return thing1['x'] == thing2['x'] and thing1['y'] == thing2['y']
 
 
-def check_collisions(robots, player):
-    for robot in robots:
-        if collided(robot, player):
+def check_collisions(robots, junk, player):
+    for thing in robots + junk:
+        if collided(thing, player):
             return True
     return False
 
@@ -93,6 +93,7 @@ def play_game():
     begin_graphics(SCREEN_WIDTH, SCREEN_HEIGHT)
     player = place_player()
     robots = place_robots(2)
+    junk = [{'shape': Box((320, 240), 10, 10, filled=True), 'x': 32, 'y': 24}]
     defeated = False
 
     while not defeated:
@@ -100,12 +101,12 @@ def play_game():
         if quit:
             break
         move_robots(robots, player)
-        defeated = check_collisions(robots, player)
+        defeated = check_collisions(robots, junk, player)
 
     if defeated:
         remove_from_screen(player['shape'])
-        for robot in robots:
-            remove_from_screen(robot['shape'])
+        for thing in robots + junk:
+            remove_from_screen(thing['shape'])
         Text("They got you!", (240, 240), size=32)
         sleep(3)
 
